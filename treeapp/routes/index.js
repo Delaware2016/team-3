2bbb815f16be95a7f0a6b6782f23f3348149f755
Riley Shaw <rlyshw@gmail.com>
2016-10-07 23:27:57 -0400

    added user router

--- treeapp/routes/index.js
+++ treeapp/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  if(req.user)console.log("WE HAVE A USER!", req.user);
   res.render('index', { title: 'Express' });
 });
 
