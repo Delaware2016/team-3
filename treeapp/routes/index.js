14c75858747d7a733f9b456f3b2df91ed4c461be
Riley Shaw <rlyshw@gmail.com>
2016-10-08 09:09:31 -0400

    redirect unauthed users to login

--- treeapp/routes/index.js
+++ treeapp/routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  if(!req.user){
+    res.redirect('/login');
+    return;
+  }
   if(req.user)console.log("WE HAVE A USER!", req.user[0]);
   console.log(req.user[0]);
   res.render('index', { title: 'Express', user: req.user[0] });
