dde5efdd6d2f37cc4c61a695259586860dfa6283
Riley Shaw <rlyshw@gmail.com>
2016-10-08 09:08:06 -0400

    try to push user to jade

--- treeapp/routes/index.js
+++ treeapp/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if(req.user)console.log("WE HAVE A USER!", req.user);
+  if(req.user)console.log("WE HAVE A USER!", req.user[0]);
+  console.log(req.user[0]);
   res.render('index', { title: 'Express', user: req.user[0] });
 });
 
