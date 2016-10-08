66607075360b8799cbf3e51b3ead891e51a4e8c6
Riley Shaw <rlyshw@gmail.com>
2016-10-08 08:51:53 -0400

    i can't javascript

--- treeapp/routes/index.js
+++ treeapp/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if(req.user)console.log("WE HAVE A USER!", req.user);
-  res.render('index', { title: 'Express', user = req.user });
+  res.render('index', { title: 'Express', user: req.user });
 });
 
 module.exports = router;
