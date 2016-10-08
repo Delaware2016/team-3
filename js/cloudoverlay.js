0ab9831c638601a6a17b2045d6496c0fa43ce3ef
Jae Yoo <yooj@udel.edu>
2016-10-08 06:05:04 -0400

    minor fixes

--- js/cloudoverlay.js
+++ js/cloudoverlay.js
@@ -19,12 +19,12 @@ function Cloud() {
  * Cloud overlay.
  * @constructor
  * @param {Array} markers Initial array of markers.
- * @param {Map} map Map to put overlay on.
+ * @param {Map} Map to put overlay on.
  */
 function CloudOverlay(markers, map) {
   this.markers_ = markers || [];
-
   this.numClouds_ = 8;
+
   this.clouds_ = [];
   this.setupClouds_();
 
@@ -82,14 +82,15 @@ CloudOverlay.prototype.draw = function() {
     return;
   }
   var overlayProjection = this.getProjection();
-  for (var i = 0; i < this.markers_.length; i++) { // from 0 to the end of this marker
+  for (var i = 0; i <= this.markers_.length; i++) { // from 0 to the end of this marker
     if (this.markers_[i]) {
       var cloud = this.clouds_[i];
+      //var latlng = this.markers_[i].getPosition();
       var latlng = this.markers_[i].getPosition();
       var div = cloud.div;
-
       var radius = 170 - (this.markers_.length - i) * 20;
       var center = overlayProjection.fromLatLngToDivPixel(latlng);
+      
       var left = center.x - radius / 2;
       var top = center.y - radius / 2;
       div.style.left = left + 'px';
@@ -106,6 +107,26 @@ CloudOverlay.prototype.draw = function() {
       for (var j = 0; j < cloud.stars.length; j++) {
         cloud.stars[j].remove();
       }
+
+    var canvas = cloud.canvas;
+    var numStars = Math.floor(cloud.radius / 40);
+    cloud.stars = [];
+    for (var j = 0; j < numStars; j++) {
+      window.setTimeout(function() {
+         if (true) {
+           var randX = Math.floor(Math.random() * (cloud.radius - 80));
+           var randY = Math.floor(Math.random() * (cloud.radius - 80));
+
+           var star = canvas.g.star(randX + 40, randY + 40, 8);
+           star.attr({stroke: '#000', fill: '90-#f7df1e-#f7df1e'});
+           star.animate({scale: '0.25 0.25', rotation: 180, opacity: 0},
+               500 + 2000 * Math.random(), function() {this.remove();});
+           cloud.stars.push(star);
+         }
+       }, j * 300);
+    }
+
+
     }
   }
   this.animateStars();
@@ -116,23 +137,28 @@ CloudOverlay.prototype.draw = function() {
  */
 CloudOverlay.prototype.animateStars = function() {
   var me = this;
-  var currentNum = this.markers_.length - 1;
-  var cloud = this.clouds_[currentNum];
-  var canvas = cloud.canvas;
-  var numStars = Math.floor(cloud.radius / 40);
-  cloud.stars = [];
-  for (var j = 0; j < numStars; j++) {
-    window.setTimeout(function() {
-       if ((me.markers_.length - 1) == currentNum) {
-         var randX = Math.floor(Math.random() * (cloud.radius - 80));
-         var randY = Math.floor(Math.random() * (cloud.radius - 80));
-         var star = canvas.g.star(randX + 40, randY + 40, 8);
-         star.attr({stroke: 'none', fill: '90-#f7df1e-#f7df1e'});
-         star.animate({scale: '0.25 0.25', rotation: 180, opacity: 0},
-             500 + 2000 * Math.random(), function() {this.remove();});
-         cloud.stars.push(star);
-       }
-     }, j * 300);
+  for (var i = 0; i < this.markers_.length; i++) {
+    //var currentNum = this.markers_.length - 1;
+    var cloud = this.clouds_[i];
+    //var cloud = this.clouds_[currentNum];
+    var canvas = cloud.canvas;
+    var numStars = Math.floor(cloud.radius / 40);
+    cloud.stars = [];
+    for (var j = 0; j < numStars; j++) {
+      window.setTimeout(function() {
+          //if ((me.markers_.length - 1) == currentNum) {
+         if (true) {
+           var randX = Math.floor(Math.random() * (cloud.radius - 80));
+           var randY = Math.floor(Math.random() * (cloud.radius - 80));
+
+           var star = canvas.g.star(randX + 40, randY + 40, 8);
+           star.attr({stroke: '#000', fill: '90-#f7df1e-#f7df1e'});
+           star.animate({scale: '0.25 0.25', rotation: 180, opacity: 0},
+               500 + 2000 * Math.random(), function() {this.remove();});
+           cloud.stars.push(star);
+         }
+       }, j * 300);
+    }
   }
 };
 
