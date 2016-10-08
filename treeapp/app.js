97b8ee217d417853a8447c7c576b01e36a355a37
Riley Shaw <rlyshw@gmail.com>
2016-10-07 23:20:17 -0400

    initialize passport

--- treeapp/app.js
+++ treeapp/app.js
@@ -29,6 +29,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', routes);
 app.use('/users', users);
